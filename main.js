719861d8e2cc900b9dde9731aca2300a61a16fb9
WalberMelo <84498612+WalberMelo@users.noreply.github.com>
2021-11-04 12:33:33 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ const api = {
   base: token.base,
 };
 
-// const api = {
-//   key: "1f01e8abc1dec3a68ad2f1a88520f500",
-//   base: "https://api.openweathermap.org/data/2.5/",
-// };
 
 const searchBox = document.querySelector(".search-box");
 searchBox.addEventListener("keypress", setQuery);
